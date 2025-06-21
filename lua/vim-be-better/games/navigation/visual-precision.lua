92df6f425c84b5c23fd80c44e8ff91f01611e061
Szymon Wilczek <kazikwilczek7@gmail.com>
2025-06-21 19:53:26 +0200

    new api usage in visual-precision game

--- lua/vim-be-better/games/navigation/visual-precision.lua
+++ lua/vim-be-better/games/navigation/visual-precision.lua
@@ -287,7 +287,7 @@ end
 function VisualOperationsRound:getConfig()
     vim.schedule(function()
         if self.window and self.window.bufh then
-            vim.api.nvim_buf_set_option(self.window.bufh, 'modifiable', true)
+            vim.bo[self.window.bufh].modifiable = true
         end
     end)
 
